401bdc62132a39fe025a77bfcef937c1e05e1bdf
hakan çakır <baranc3363@gmail.com>
2022-02-23 07:29:45 +0300

    Add project files.

---
+++ FluentVal/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FluentVal.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
