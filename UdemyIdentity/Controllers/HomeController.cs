5d89d5001a788031f6e374a856ec48d5c8eb211c
Hakan Baran Çakir <baranc3363@gmail.com>
2022-03-29 20:22:58 +0300

    viewmodel

--- UdemyIdentity/Controllers/HomeController.cs
+++ UdemyIdentity/Controllers/HomeController.cs
@@ -1,10 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using UdemyIdentity.Models;
+using UdemyIdentity.ViewModels;
 
 namespace UdemyIdentity.Controllers
 {
     public class HomeController : Controller
     {
+        private UserManager<AppUser> userManager { get; }
+
+        public HomeController(UserManager<AppUser> userManager)
+        {
+            this.userManager = userManager;
+        }
         public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult SignUp(UserViewModel user)
         {
             return View();
         }
---
+++ UdemyIdentity/ViewModels/UserViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UdemyIdentity.ViewModels
+{
+    public class UserViewModel
+    {
+        [Required(ErrorMessage ="Kullanıcı Adı Gereklidir")]
+        [Display(Name ="Kullanıcı Adı :")]
+        public string UserName{ get; set; }
+
+        [Display(Name = "Tel No:")]
+        public string PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "Email Adresi Gereklidir")]
+        [Display(Name = "Email Adresi:")]
+        [EmailAddress(ErrorMessage ="Hatalı Mail Girişi")]
+        public string Email { get; set; }
+        
+        [Required(ErrorMessage ="Şifre Zorunludur")]
+        [Display(Name = "Şifre :")]
+        [DataType(DataType.Password)]// emailde bu sekilde yazılabilir
+        public string Password { get; set; }
+
+    }
+}
