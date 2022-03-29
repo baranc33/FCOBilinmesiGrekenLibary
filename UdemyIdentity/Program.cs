9aa49ddecf48e810b08e0ef46577d6df4193f293
Hakan Baran Çakir <baranc3363@gmail.com>
2022-03-29 15:49:28 +0300

    updgrade

--- UdemyIdentity/Program.cs
+++ UdemyIdentity/Program.cs
@@ -2,6 +2,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddMvc();
 
 var app = builder.Build();
+app.UseDeveloperExceptionPage();// developer hata mesajlarý
+app.UseStatusCodePages();// özellikle bir content dönmiyen sayfalarda hata içeriði
+app.UseStaticFiles();
 
 app.MapControllerRoute(
     name: "default",
