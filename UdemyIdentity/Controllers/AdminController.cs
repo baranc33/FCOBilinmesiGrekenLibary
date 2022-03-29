19af56285af720bf9cb4d8eceea9019b67120eef
Hakan Baran Çakir <baranc3363@gmail.com>
2022-03-29 20:14:06 +0300

    usermanager list

---
+++ UdemyIdentity/Controllers/AdminController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using UdemyIdentity.Models;
+
+namespace UdemyIdentity.Controllers
+{
+    public class AdminController : Controller
+    {
+        private UserManager<AppUser> userManager { get; }
+
+        public AdminController(UserManager<AppUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+
+
+        public IActionResult Index()
+        {
+            //IQueryable qb = userManager.Users;
+            // belirli adet alabiliriz
+            return View(userManager.Users.ToList());
+            // kullanıcı listesi Döner
+
+        }
+
+    }
+}
