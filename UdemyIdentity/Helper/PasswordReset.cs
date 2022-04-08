6e0579f9752c2384c84bff5e60259589b7115491
Hakan Baran Çakir <baranc3363@gmail.com>
2022-04-08 13:10:09 +0300

    last passwordResett

--- UdemyIdentity/Helper/PasswordReset.cs
+++ UdemyIdentity/Helper/PasswordReset.cs
@@ -9,29 +9,45 @@ namespace UdemyIdentity.Helper
 
         public static void PasswordResetSendEmail(string link, string email)
         {
+        // mail göndere bilmek önce mail açıkken güvenliğini izin vermeliyiz
+        //https://myaccount.google.com/lesssecureapps?pli=1&rapt=AEjHL4NdWDCEVH_yZNeXLR5Ca9EaqYQ3lgEYU5tyIERrr49koSOULHvyneHuKOsV2nSfPj0zkSoesKO5LOunNMzbJCgxSOSG2Q
 
-            var fromAddress = new MailAddress("docenthesapla@gmail.com", "From Name");
-            var toAddress = new MailAddress(email, "To Name");
-            const string subject = "test";
-            const string body = "Hey now!!";
-
-            var smtp = new SmtpClient
+            try
             {
-                Host = "smtp.gmail.com",
-                Port = 587,
-                EnableSsl = true,
-                DeliveryMethod = SmtpDeliveryMethod.Network,
-                Credentials = new NetworkCredential("docenthesapla@gmail.com", "hesaplaDocent33"),
-            Timeout = 20000
-            };
-            using (var message = new MailMessage(fromAddress, toAddress)
-            {
-                Subject = subject,
-                Body = body
-            })
-            {
-                smtp.Send(message);
+                //MailMessage kütüphanesinden bir instance oluşturuyoruz.
+                MailMessage mail = new MailMessage();
+                //Mesaj içeriğinde html ifadelere izin veriyoruz.
+                mail.IsBodyHtml = true;
+                //Bu kısım mail'in kime gideceğidir.Kendi adresimi yazdım.
+                mail.To.Add(email);
+                //Burası ise kimin göndereceğidir.Kim gönderecek?
+                mail.From = new MailAddress("docenthesapla@gmail.com","Doçent Hesaplama");
+                //Gelen mailin konusu
+                mail.Subject = "Şifre Sıfırlama isteği";
+                mail.Body = "<h2>Doçentlik Puan Hesaplama sitemize Şifre yenilenmesine dair bir istek atıkldı</h2>";
+                mail.Body += "<h4>Eğer Bu isteği siz göndermediyseniz ciddiye almayınız</h4><hr/>";
+                mail.Body += $"<a style='color:red;text-decoration:none; ' href='{link}'>Şifrenizi Yenilemek için tıklayınız.</a>";
+                mail.IsBodyHtml = true;
+                // smptp clientiını host firmamızdan öğreneceğiz
+                SmtpClient smtp = new SmtpClient();
+                //Burada maili gönderen kişinin mail adresi ve şifresi alınıyor.
+                smtp.Credentials = new System.Net.NetworkCredential("docenthesapla@gmail.com", "hesaplaDocent33");
+                //Hangi portu kullanacağımızı yazıyoruz.
+                smtp.Port = 587;
+                //Hangi mail adresini kullanacağızı seçiyoruz.
+                smtp.Host = "smtp.gmail.com";
+                //Ssl güvenlik protokolünü aktifleştiriyoruz.
+                smtp.EnableSsl = true;
+                //Maili gönderiyoruz.
+                smtp.Send(mail);
+            
             }
+            catch (Exception)
+            {
+                throw;
+            }
+           
+ 
         }
         //public static void PasswordResetSendEmail(string link, string email)
         //{
@@ -53,49 +69,6 @@ namespace UdemyIdentity.Helper
         //    smtpClient.Send(mail);
 
         //}
-
-
-        //public static void PasswordResetSendEmail(string link, string email)
-        //{
-        //    //Gelen bilgileri string değişkenlere attım.
-        //    //string mailadress = email;
-
-        //    //try bloğu içerinde yazdım sorun çıkar ise çözüm üreteyim diye.
-        //    try
-        //    {
-        //        //MailMessage kütüphanesinden bir instance oluşturuyoruz.
-        //        MailMessage mail = new MailMessage();
-        //        //Mesaj içeriğinde html ifadelere izin veriyoruz.
-        //        mail.IsBodyHtml = true;
-        //        //Bu kısım mail'in kime gideceğidir.Kendi adresimi yazdım.
-        //        mail.To.Add(email);
-        //        //Burası ise kimin göndereceğidir.Kim gönderecek?
-        //        mail.From = new MailAddress(email);
-        //        //Gelen mailin konusu
-        //        mail.Subject = $"www.DocentHesapla.com::Şifre sıfırlama";
-        //        mail.Body = "<h2>Şifrenizi yenilemek için lütfen aşağıdaki linke tıklayınız.</h2><hr/>";
-        //        mail.Body += $"<a href='{link}'>şifre yenileme linki</a>";
-        //        //Gelen mailin içeriği
-        //        mail.IsBodyHtml = true;
-        //        //Bu kısımda smtp classında instance oluşturuyoruz.
-        //        SmtpClient smtp = new SmtpClient();
-        //        //Burada maili gönderen kişinin mail adresi ve şifresi alınıyor.
-        //        smtp.Credentials = new NetworkCredential("DocentHesapla@gmail.com", "hesaplaDocent33");
-        //        //Hangi portu kullanacağımızı yazıyoruz.
-        //        smtp.Port = 587;
-        //        //Hangi mail adresini kullanacağızı seçiyoruz.
-        //        smtp.Host = "smtp.gmail.com";
-        //        //Ssl güvenlik protokolünü aktifleştiriyoruz.
-        //        smtp.EnableSsl = true;
-        //        //Maili gönderiyoruz.
-        //        smtp.Send(mail);
-        //        //return Json(true, JsonRequestBehavior.AllowGet);
-        //    }
-        //    catch (Exception)
-        //    {
-        //        throw;
-        //    }
-
-        //}
+          
     }
 }
